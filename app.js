4ae21fae96415a5b921d081f9a2c561fe4d77942
chinazhangyujia <chinazhangyujia@gmail.com>
2017-03-31 12:45:38 -0400

    commit

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
+var http = require('http');
+var websockets = require('socket.io');
 var express = require('express');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 var customRouter = require('./router');
 var PORT = 8000;
-var app = express();
+//var app = express();
+var app = setupExpress();
 
-// use URL-encoded parser as a top-level middleware, which will parse the bodies of all incoming requests.
-app.use(urlencodedParser);
+listen(function (socket, sockets){
+  socket.on('chat', function(data){
+    console.log(data.target);
+  });
+})
 
-// serve static file such as image, now you can use img in views by the url '/static/Admirals.PNG'
-app.use('/static', express.static('static'));
 
-app.use('/', customRouter);
-app.get('/', function(req, res) {
-  // views folder is recommended for html view file
-  res.sendFile('./index.html', { root: __dirname + '/views/' });
-});
+function listen(callback){
+  var sockets = [];
+  var server = http.createServer(app);
+  var io = websockets.listen(server);
+  io.sockets.on('connection', function(socket){
+    console.log('a user is connected');
+    sockets.push(socket);
+    callback(socket, sockets);
+  });
+}
 
-// experss can create http server itself
-app.listen(PORT, function(){
-  console.log('listening port '+ PORT);
-});
+function setupExpress(){
+  var app = express();
+  // use URL-encoded parser as a top-level middleware, which will parse the bodies of all incoming requests.
+  app.use(urlencodedParser);
+
+  // serve static file such as image, now you can use img in views by the url '/static/Admirals.PNG'
+  app.use('/static', express.static('static'));
+
+  app.use('/', customRouter);
+  app.get('/', function(req, res) {
+    // views folder is recommended for html view file
+    res.sendFile('./index.html', { root: __dirname + '/views/' });
+  });
+
+  // experss can create http server itself
+  app.listen(PORT, function(){
+    console.log('listening port '+ PORT);
+  });
+
+  return app;
+}
